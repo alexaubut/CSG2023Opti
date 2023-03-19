8100daa55285c70f0cf494f27296ca3c6f292747
Alexandre <alexaubut62@gmail.com>
2023-03-18 20:24:08 -0400

    modified

--- optmiser.py
+++ optmiser.py
@@ -3,24 +3,21 @@ from download_yahoo import Day, Title
 
 class Optimisier():
     def get_transactions(self, data: list[Title]) -> list[Transaction]:
-        #TODO gérer premier achat
-        first_title = None
+        active_title = None
         transactions = []
         for day in data[0].days[:-2]:
-            transaction, new_title = self.get_best_transaction(data, first_title, day.date)
+            transaction, new_title = self.get_best_transaction(data, active_title, day.date)
             if transaction:
-                first_title = new_title
+                active_title = new_title
                 transactions.append(transaction)
-        if first_title:
-            transactions.append(Transaction(data[0].days[-1].date, "SELL", first_title.title))
+        if active_title:
+            transactions.append(Transaction(data[0].days[-1].date, "SELL", active_title.title))
         return transactions
-        #TODO gérer 2 derniers jours index error
-
 
     def get_delta(self, first_day:Day, second_day:Day) -> float:
-        return second_day.open - first_day.open
+        return second_day.open / first_day.open
     
-    def get_best_transaction(self, data: list[Title], active_title: Title or None, day: str) -> tuple(Transaction or None, Title or None):
+    def get_best_transaction(self, data: list[Title], active_title: Title or None, day: str) -> Transaction or None:
         delta_active_title = None
         if active_title:
             delta_active_title = self.get_delta(active_title.get_date(day), active_title.days[active_title.get_date_by_index(day) + 2] )
