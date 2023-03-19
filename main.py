ad650684961d4b51e00f4ca4873efbaead638be8
Alexandre <alexaubut62@gmail.com>
2023-03-18 20:53:26 -0400

    final submit

--- main.py
+++ main.py
@@ -2,21 +2,10 @@ from download_yahoo import DownloadYahoo
 from optmiser import Optimisier
 
 if __name__ == "__main__":
-    title = ["AAL", "DAL", "UAL", "LUV", "HA"]
+    #test_title = ["AAL", "DAL", "UAL", "LUV", "HA"]
+    final_title = ["GOOG", "AMZN", "META", "MSFT", "AAPL"]
     start_date = "2023-01-01"
     end_date = "2023-02-01"
-    data = DownloadYahoo(title, start_date, end_date).get_data()
+    data = DownloadYahoo(final_title, start_date, end_date).get_data()
     transactions = Optimisier().get_transactions(data)
     print(transactions)
-    amount = 1000000
-    stock = 0
-    for transaction in transactions:
-        # print(transaction)
-        if transaction.action == "BUY":
-            stock = amount / transaction.day.open
-            amount = 0
-        else:
-            amount += transaction.day.open * stock
-            stock = 0
-        print(amount)
-    print(data[0].days[0].date)
--- optmiser.py
+++ optmiser.py
@@ -5,7 +5,7 @@ class Optimisier():
     def get_transactions(self, data: list[Title]) -> list[Transaction]:
         active_title = None
         transactions = []
-        for day in data[0].days[:-2]:
+        for day in data[0].days[:-4]:
             transaction, new_title = self.get_best_transaction(data, active_title, day)
             if transaction:
                 active_title = new_title
@@ -19,19 +19,30 @@ class Optimisier():
     
     def get_best_transaction(self, data: list[Title], active_title: Title or None, day_obj: Day) -> tuple[Transaction or None, Title or None]:
         day = day_obj.date
-        delta_active_title = None
+        delta_active_title_2_days = None
+        delta_active_title_3_days = None
         if active_title:
-            delta_active_title = self.get_delta(active_title.get_date(day), active_title.days[active_title.get_date_by_index(day) + 2] )
+            delta_active_title_2_days = self.get_delta(active_title.get_date(day), active_title.days[active_title.get_date_by_index(day) + 2] )
+            delta_active_title_3_days = self.get_delta(active_title.get_date(day), active_title.days[active_title.get_date_by_index(day) + 3] )
         best_delta = -999999999
         best_title = None
         for title in data:
-            delta = self.get_delta(title.days[title.get_date_by_index(day) + 1], title.days[title.get_date_by_index(day) + 2])
-            if delta > best_delta:
-                best_title = title
-                best_delta = delta
-        if delta_active_title:
-            if delta_active_title < 0 or delta_active_title < best_delta:
+            delta_3 = self.get_delta(title.days[title.get_date_by_index(day) + 1], title.days[title.get_date_by_index(day) + 3])
+            delta_2 = self.get_delta(title.days[title.get_date_by_index(day) + 1], title.days[title.get_date_by_index(day) + 2])
+            if delta_2 > best_delta or delta_3 > best_delta:
+                if delta_2 > delta_3:
+                    best_title = title
+                    best_delta = delta_2
+                else:
+                    best_title = title
+                    best_delta = delta_3
+        if active_title:
+            if ((delta_active_title_2_days < 0 or delta_active_title_2_days < best_delta) and
+                (delta_active_title_3_days < 0 or delta_active_title_3_days < best_delta)):
                 return (Transaction(day_obj, "SELL", active_title.title), None)
             else:
                 return None, None
-        return (Transaction(day_obj, "BUY", best_title.title), best_title)
+        if best_delta > 0:
+            return (Transaction(day_obj, "BUY", best_title.title), best_title)
+        else:
+            return None, None
